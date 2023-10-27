04b410bc63abb505d0a871ad95d9f7f4a8d167e6
Bruno dos Reis Sola <brunosola3@gmail.com>
2023-10-27 20:04:50 -0300

    test

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ import model.entities.Seller;
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        Seller seller = sellerDao.findById(3);
+        Seller seller = sellerDao.findById(2);
 
         System.out.println(seller);
     }
